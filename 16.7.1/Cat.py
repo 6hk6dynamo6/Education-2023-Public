11ed85a88cc58eb2f505f9f18424e796e279cc10
6hk6dynamo6 <126092207+6hk6dynamo6@users.noreply.github.com>
2023-05-12 12:29:47 +0300

    Add files via upload

---
+++ 16.7.1/Cat.py
@@ -0,0 +1,9 @@
+class Cat:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def pet(self):
+        print(self.name, self.gender, self.age)
+
---
+++ 16.7.1/CatTest.py
@@ -0,0 +1,7 @@
+from Cat import Cat
+
+cat1 = Cat("Барон", "Мальчик", "2")
+cat2 = Cat("Сэм", "Мальчик", "2")
+
+cat1.pet()
+cat2.pet()
